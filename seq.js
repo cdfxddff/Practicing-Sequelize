8ccddf7ad4811f4844b493516195bca5a93a73a2
yamaguchi <16e4q8yymg@gmail.com>
2022-09-30 21:52:24 +0900

    形式的なお直し

--- seq.js
+++ seq.js
@@ -1,15 +1,40 @@
-const {Sequelize}=require('sequelize');
+const {Sequelize,DataTypes}=require('sequelize');
 
-const sequelize=new Sequelize('shoppingapp','postgres','password',{
+const sequelize=new Sequelize('seq','postgres','password',{
     host:'localhost',
     dialect:'postgres'
 });//即時関数の前の処理では;が必要
 
-(async function(){
-    try{
-        await sequelize.authenticate();//接続できているかどうかの確認
-        console.log('Connection has been established successfully.');
-    }catch(error){
-        console.error('Unable to connect to the database:', error);
+const Text=sequelize.define('text',{
+    text:{
+        type:DataTypes.STRING,
+        allowNull:false
     }
+});
+(async function(){
+    await Text.sync();
 })();
+
+//これはCREATEの関数にする
+async function dbcre(inp_text){
+    await Text.create({text:inp_text});
+}
+
+async function dbdrop(inp_id) { 
+    await Text.destroy({
+        where:{
+            id:inp_id
+        }
+    });
+}
+
+async function dbsel() { 
+    const data = await Text.findAll();
+    return data;
+ }
+
+module.exports={
+    cre:dbcre,
+    drp:dbdrop,
+    sel:dbsel,
+}
